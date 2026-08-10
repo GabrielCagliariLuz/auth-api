d35fdfd3d5eff7672ba441f84b991c66d15edb8e
Gabriel Cagliari Luz <gabrielluz.git.hub@gmail.com>
2026-08-10 11:43:14 -0300

    feat: adiciona Spring Security, BCryptPasswordEncoder e criptografia de senha no cadastro

---
+++ src/main/java/com/auth/api/config/SecurityConfigurations.java
@@ -0,0 +1,28 @@
+package com.auth.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurations {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(req -> req.anyRequest().permitAll()) // Libera todas as rotas por enquanto
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(); // Injeta o BCrypt no projeto
+    }
+
+}
--- src/main/java/com/auth/api/service/UsuarioService.java
+++ src/main/java/com/auth/api/service/UsuarioService.java
@@ -4,6 +4,7 @@ import com.auth.api.usuario.DadosCadastroUsuario;
 import com.auth.api.usuario.Usuario;
 import com.auth.api.usuario.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,6 +14,9 @@ public class UsuarioService {
     @Autowired
     private UsuarioRepository repository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Transactional
     public Usuario cadastrar(DadosCadastroUsuario dados){
         if (!dados.senha().equals(dados.confirmarSenha())){
@@ -21,7 +25,8 @@ public class UsuarioService {
         if (repository.existsByEmail(dados.email())){
             throw new IllegalArgumentException("Este e-mail já está cadastrado.");
         }
-        Usuario usuario = new Usuario(dados);
+        String senhaCriptografada = passwordEncoder.encode(dados.senha());
+        Usuario usuario = new Usuario(null, dados.nome(), dados.email(), senhaCriptografada);
         return repository.save(usuario);
     }
 }

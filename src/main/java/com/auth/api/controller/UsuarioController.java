2dffff5a2b7fd15a3261c9f344d1139aa433893d
Gabriel Cagliari Luz <gabrielluz.git.hub@gmail.com>
2026-08-10 10:47:08 -0300

    feat: cria UsuarioController com endpoint POST /usuarios para cadastro

---
+++ src/main/java/com/auth/api/controller/UsuarioController.java
@@ -0,0 +1,26 @@
+package com.auth.api.controller;
+
+import com.auth.api.usuario.DadosCadastroUsuario;
+import com.auth.api.usuario.Usuario;
+import com.auth.api.usuario.UsuarioRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioRepository repository;
+
+    @PostMapping
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroUsuario dados){
+        repository.save(new Usuario(dados));
+    }
+}

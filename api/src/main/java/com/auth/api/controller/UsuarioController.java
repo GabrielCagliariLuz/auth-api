79d251c1609ec91339dff765f0381947143579a3
Gabriel Cagliari Luz <gabrielluz.git.hub@gmail.com>
2026-08-18 22:27:19 -0300

    chore(repo): reorganizar estrutura para monorepo unificando api e frontend na raiz

--- api/src/main/java/com/auth/api/controller/UsuarioController.java
+++ api/src/main/java/com/auth/api/controller/UsuarioController.java
@@ -6,7 +6,6 @@ import com.auth.api.usuario.DadosCadastroUsuario;
 import com.auth.api.usuario.DadosDetalhamentoUsuario;
 import com.auth.api.usuario.Usuario;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
